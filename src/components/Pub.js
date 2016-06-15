70e0073f3dfaca8e45acbe6f0656ca668fc2556a
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2016-06-15 20:50:44 +0200

    Pub styles

--- src/components/Pub.js
+++ src/components/Pub.js
@@ -4,6 +4,7 @@ import {
     ScrollView,
     View,
     Text,
+    StyleSheet,
 } from 'react-native';
 
 import * as actions from '../actions';
@@ -11,19 +12,89 @@ import * as selectors from '../selectors';
 
 
 const PubComponent = ({pub, taps}) => (
-    <ScrollView style={{flex: 1, backgroundColor: 'pink'}}>
-        <View>
-            <Text>{pub.name}</Text>
-            {taps.map((t) => <Tap tap={t}/>)}
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+        <View style={styles.titleWrapper}>
+            <Text style={styles.title}>{pub.name}</Text>
         </View>
-    </ScrollView>
+        <ScrollView style={{flex: 1}}>
+            {taps.map((t) => <Tap tap={t}/>)}
+        </ScrollView>
+    </View>
 );
 
 const Tap = ({tap}) => (
-    <View>
-        <Text>{tap.beer ? tap.beer.name : 'pusty'}</Text>
+    <View style={styles.tap}>
+        <Text style={styles.tapName}>{tap.tapName}</Text>
+        <Beer beer={tap.beer}/>
     </View>
 );
+
+const Beer = ({beer}) => (
+    <View>
+        <Text style={styles.beerName}>{beer.name}</Text>
+        <Text style={styles.beerStyleName}>{beer.style}</Text>
+        <Text style={styles.beerBreweryName}>{beer.brewery}</Text>
+        <View style={styles.detailsWrapper}>
+            <View style={styles.detailWrapper}>
+                <Text style={styles.detailsName}>ABV</Text>
+                <Text style={styles.detailsValue}>{beer.abv ? beer.abv : '--'}</Text>
+            </View>
+            <View style={styles.detailWrapper}>
+                <Text style={styles.detailsName}>IBU</Text>
+                <Text style={styles.detailsValue}>{beer.ibu ? beer.ibu : '--'}</Text>
+            </View>
+        </View>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    titleWrapper: {
+        borderBottomWidth: 0.5,
+        borderColor: 'gray',
+    },
+    title: {
+        margin: 16,
+        fontSize: 24,
+    },
+    tap: {
+        borderBottomWidth: 0.5,
+        borderColor: 'gray',
+        padding: 16,
+    },
+    tapName: {
+        fontSize: 10,
+        color: 'gray',
+    },
+    beerName: {
+        fontSize: 18,
+        color: 'black',
+    },
+    beerStyleName: {
+        fontSize: 14,
+        color: '#333',
+    },
+    beerBreweryName: {
+        fontSize: 14,
+        color: '#333',
+    },
+    detailsWrapper: {
+        flexDirection: 'row',
+    },
+    detailWrapper: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        marginRight: 8,
+    },
+    detailsName: {
+        fontSize: 12,
+        color: '#333',
+        marginRight: 4,
+    },
+    detailsValue: {
+        fontSize: 14,
+        color: '#333',
+    },
+});
 
 class PubContainer extends React.Component {
     componentWillMount() {
