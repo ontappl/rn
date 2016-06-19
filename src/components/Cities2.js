ecd5fb65fb25e3cb7400c9adeb256d45c508a99f
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2016-06-19 19:23:46 +0200

    First step to fix macOS caseinsensitive filename problems...

---
+++ src/components/Cities2.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+    StyleSheet,
+    ScrollView,
+    Text,
+    View,
+    TouchableNativeFeedback,
+} from 'react-native';
+import {connect} from 'react-redux';
+
+import * as actions from '../actions';
+import * as navigationActions from '../navigation/actions';
+
+class CitiesContainer extends React.Component {
+    componentDidMount() {
+        this.props.fetchCities();
+    }
+
+    render() {
+        const {isLoading, citiesObject} = this.props;
+        const cities = Object
+            .keys(citiesObject)
+            .map((k) => citiesObject[k])
+            .sort((a, b) => a.name.localeCompare(b.name));
+        return (
+            <ScrollView style={{flex: 1, marginTop: 56}}>
+                {cities.length > 0 && cities.map(c =>
+                    <CityButton
+                        key={c.id}
+                        city={c}
+                        onPress={this._onCityPress.bind(this)}
+                    />
+                )
+                }
+            </ScrollView>
+        );
+    }
+
+    _onCityPress(city) {
+        const state = {key: 'city', city, title: city.name};
+        this.props.selectCity(state);
+    }
+}
+
+const CityButton = ({city, onPress}) => (
+    <TouchableNativeFeedback onPress={() => onPress(city)}>
+        <View style={{padding: 16}}>
+            <Text>{city.name}</Text>
+        </View>
+    </TouchableNativeFeedback>
+);
+
+const mapStateToProps = ({cities}) => ({
+    isLoading: cities.isLoading,
+    citiesObject: cities.cities,
+    error: cities.error,
+});
+
+const mapActions = {
+    fetchCities: actions.fetchCitiesRequest,
+    selectCity: navigationActions.push,
+};
+
+export const Cities = connect(mapStateToProps, mapActions)(CitiesContainer);
