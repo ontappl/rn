78264de495cbd279d1198250777216f6282f7145
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2016-07-04 18:13:05 +0200

    Beers placeholder

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -2,15 +2,29 @@ import React from 'react';
 import {
     View,
     Text,
+    StyleSheet,
 } from 'react-native';
 
 
 export const Beers = ({city}) => (
-    <View style={{flex: 1, backgroundColor: 'green'}}>
-        <Text>------ Beers ------</Text>
-        <Text>------ Beers ------</Text>
-        <Text>------ Beers ------</Text>
-        <Text>------ Beers ------</Text>
-        <Text>------ Beers ------</Text>
+    <View style={styles.wrapper}>
+        <Text style={styles.text}>
+            Tutaj znajdzie się wyszukiwarka i lista piw.
+        </Text>
     </View>
 );
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: 60,
+        paddingRight: 60,
+    },
+    text: {
+        fontSize: 16,
+        textAlign: 'center',
+    },
+});
